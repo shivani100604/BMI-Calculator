3f7e93b69f775e770fa1e7d4da4c8024cae2900b
Shivani <156783311+shivani100604@users.noreply.github.com>
2025-07-11 12:53:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
+
+
+   
+
